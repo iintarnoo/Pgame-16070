bed85b70b207ff441074561f526eee368a1708d5
iintarnoo <56122084+iintarnoo@users.noreply.github.com>
2019-10-03 23:31:57 +0900

    tugas 2 pemrograman game

---
+++ code_tugas2.py
@@ -0,0 +1,8 @@
+# Tugas Ke2. MK Pemrograman Game
+
+print("Halo nama saya Iin Tarno")
+print("Halo angkatan 2016")
+print("Halo Teknik Info UMMU Ternate")
+#baris ini tidak akan dieksekusi
+print ("Halo Anak Info....") # baris ini dieksekui
+#Baris ini juga merupakan Baris Komentar
